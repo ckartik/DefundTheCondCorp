fdbe70798e05e2a66945f711b3033f9874552d7f
zKjet <100480520+zkjet@users.noreply.github.com>
2022-08-10 00:20:30 -0400

    solved npm run dev issue updated package.json

--- .next/server/pages/index.js
+++ .next/server/pages/index.js
@@ -1,32 +1,35 @@
 "use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
 (() => {
 var exports = {};
-exports.id = 405;
-exports.ids = [405];
+exports.id = "pages/index";
+exports.ids = ["pages/index"];
 exports.modules = {
 
-/***/ 524:
+/***/ "./pages/index.js":
+/*!************************!*\
+  !*** ./pages/index.js ***!
+  \************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-// ESM COMPAT FLAG
-__webpack_require__.r(__webpack_exports__);
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-dev-runtime */ \"react/jsx-dev-runtime\");\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__);\n\nfunction HomePage() {\n    return /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(\"div\", {\n        children: \"DefundTheCondCorp\"\n    }, void 0, false, {\n        fileName: \"/Users/zkjet/Desktop/ETHToronto/DefundTheCondCorp/pages/index.js\",\n        lineNumber: 2,\n        columnNumber: 12\n    }, this);\n}\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (HomePage);\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9wYWdlcy9pbmRleC5qcy5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7QUFBQTtBQUFBLFNBQVNBLFFBQVEsR0FBRztJQUNoQixxQkFBTyw4REFBQ0MsS0FBRztrQkFBQyxtQkFBaUI7Ozs7O1lBQU07Q0FDcEM7QUFFRCxpRUFBZUQsUUFBUSIsInNvdXJjZXMiOlsid2VicGFjazovL2hhcmRoYXQtcHJvamVjdC8uL3BhZ2VzL2luZGV4LmpzP2JlZTciXSwic291cmNlc0NvbnRlbnQiOlsiZnVuY3Rpb24gSG9tZVBhZ2UoKSB7XG4gICAgcmV0dXJuIDxkaXY+RGVmdW5kVGhlQ29uZENvcnA8L2Rpdj5cbiAgfVxuICBcbiAgZXhwb3J0IGRlZmF1bHQgSG9tZVBhZ2UiXSwibmFtZXMiOlsiSG9tZVBhZ2UiLCJkaXYiXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./pages/index.js\n");
 
-// EXPORTS
-__webpack_require__.d(__webpack_exports__, {
-  "default": () => (/* binding */ pages)
-});
+/***/ }),
 
-;// CONCATENATED MODULE: external "react/jsx-runtime"
-const jsx_runtime_namespaceObject = require("react/jsx-runtime");
-;// CONCATENATED MODULE: ./pages/index.js
+/***/ "react/jsx-dev-runtime":
+/*!****************************************!*\
+  !*** external "react/jsx-dev-runtime" ***!
+  \****************************************/
+/***/ ((module) => {
 
-function HomePage() {
-    return /*#__PURE__*/ jsx_runtime_namespaceObject.jsx("div", {
-        children: "DefundTheCondCorp"
-    });
-}
-/* harmony default export */ const pages = (HomePage);
-
+module.exports = require("react/jsx-dev-runtime");
 
 /***/ })
 
@@ -37,7 +40,7 @@ function HomePage() {
 var __webpack_require__ = require("../webpack-runtime.js");
 __webpack_require__.C(exports);
 var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
-var __webpack_exports__ = (__webpack_exec__(524));
+var __webpack_exports__ = (__webpack_exec__("./pages/index.js"));
 module.exports = __webpack_exports__;
 
 })();
