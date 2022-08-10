0d568e0ecf69e4813ebcbfc3d054963ab8297677
zKjet <100480520+zkjet@users.noreply.github.com>
2022-08-10 00:50:49 -0400

    updated font to inter

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
+// tailwind.config.js
+const defaultTheme = require('tailwindcss/defaultTheme')
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
-  plugins: [],
 }
